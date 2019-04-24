af92d3c175bed37d0c509cd389e8a123374da500
IlyaSalo <ilya.salomatin.bitmedia@gmail.com>
2019-04-24 11:30:40 +0300

    UpdateUnitTest2

--- NUnitTest/UnitTest1.cs
+++ NUnitTest/UnitTest1.cs
@@ -7,16 +7,17 @@ using NUnit.Framework;
 
 namespace Tests
 {
+    [TestFixture]
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
-           
+            Thread.Sleep(1000);
         }
 
         [Test]
-        public void Test1()
+        public void TestTime()
         {
             Random rnd = new Random();
             var sorter = new Context(new BubbleSort());
@@ -49,9 +50,37 @@ namespace Tests
             var bubbleSortTime = watchBubbleSort.ElapsedMilliseconds;
             var selectionSortTime = watchSelectionSort.ElapsedMilliseconds;
             var insertionSortTime = watchInsertionSort.ElapsedMilliseconds;
-
+            //throw  new Exception();
             Thread.Sleep(1000);
             //Assert.Pass();
+
+            
+        }
+
+
+
+        [Test]
+        public void TestSort()
+        {
+            Random rnd = new Random();
+            var sorter = new Context(new BubbleSort());  //  SelectionSort() InsertionSort()
+            var syze = 100;
+            int[] ar = new int[syze];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] = rnd.Next(-100, 100);
+            }
+            sorter.DoSomeSort(ar);
+
+            // Ckeck syze of array
+            if (ar.Length != syze) throw new Exception();
+
+            //Check if do not sort
+            for (var i = 0; i < ar.Length - 1; i++)
+            {
+               if (ar[i] > ar[i+1]) throw new Exception();
+            }
+            Thread.Sleep(1000);
         }
     }
 }
